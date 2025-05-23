f86280e7fb061a3ba19ddd86bfc470db71303aef
gargvishu2000 <45060213+gargvishu2000@users.noreply.github.com>
2025-05-23 11:34:46 +0530

    add cors in io

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://chat-app-htiw.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
@@ -15,7 +17,6 @@ export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
@@ -24,7 +25,6 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
